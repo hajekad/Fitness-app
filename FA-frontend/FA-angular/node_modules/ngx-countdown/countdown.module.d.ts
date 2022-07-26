b1d925ea4cac7c3bba685337e5e3bac16da41778
hajekad <22adamhajek@gmail.com>
2022-07-26 22:54:04 +0200

    chore: add libs

---
+++ FA-frontend/FA-angular/node_modules/ngx-countdown/countdown.module.d.ts
@@ -0,0 +1,7 @@
+import * as i0 from "@angular/core";
+import * as i1 from "./countdown.component";
+export declare class CountdownModule {
+    static ɵfac: i0.ɵɵFactoryDeclaration<CountdownModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<CountdownModule, never, [typeof i1.CountdownComponent], [typeof i1.CountdownComponent]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<CountdownModule>;
+}
